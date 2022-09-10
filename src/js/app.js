f9870911390b40e3f96a62039a95971b3029c428
Daniel Strunk <daniel@strunk.me>
2022-09-10 19:19:04 -0400

    Style update

--- src/js/app.js
+++ src/js/app.js
@@ -4,38 +4,38 @@ import { createRoot } from 'react-dom/client'
 const files = require.context('./', true, /\.jsx$/i, 'sync')
 const components = {}
 files.keys().map((key) => {
-  components[key.split('/').pop().split('.')[0]] = lazy(() => import(`${key}`))
+    components[key.split('/').pop().split('.')[0]] = lazy(() => import(`${key}`))
 })
 
 const hasJsonStructure = (prop) => {
-  if (typeof prop !== 'string') return false;
+    if (typeof prop !== 'string') return false;
 
-  try {
-    const result = JSON.parse(prop)
-    const type = Object.prototype.toString.call(result)
+    try {
+        const result = JSON.parse(prop)
+        const type = Object.prototype.toString.call(result)
 
-    return type === '[object Object]' || type === '[object Array]'
-  } catch (err) {
-    return false
-  }
+        return type === '[object Object]' || type === '[object Array]'
+    } catch (err) {
+        return false
+    }
 }
 
 const getPropsFromAttributes = (element) => {
-  if (element.attributes.length === 0) {
-    return null;
-  }
-
-  let props = {}
-  for (let index = 0; index < element.attributes.length; index++) {
-    let attribute = element.attributes[index]
-    if (hasJsonStructure(attribute.value)) {
-      props[attribute.name] = JSON.parse(attribute.value)
-    } else {
-      props[attribute.name] = attribute.value
+    if (element.attributes.length === 0) {
+        return null;
     }
-  }
 
-  return props;
+    let props = {}
+    for (let index = 0; index < element.attributes.length; index++) {
+        let attribute = element.attributes[index]
+        if (hasJsonStructure(attribute.value)) {
+            props[attribute.name] = JSON.parse(attribute.value)
+        } else {
+            props[attribute.name] = attribute.value
+        }
+    }
+
+    return props;
 }
 
 const walkReactDom = (el, callback, count = 0) => {
@@ -54,30 +54,30 @@ const walkReactDom = (el, callback, count = 0) => {
 }
 
 Object.entries(components).forEach(([key, value]) => {
-  const elements = document.getElementsByTagName(key)
-  if (!elements || elements.length === 0) {
-    return;
-  }
+    const elements = document.getElementsByTagName(key)
+    if (!elements || elements.length === 0) {
+        return;
+    }
 
-  let Component = value
-  for (let i = elements.length - 1; i >= 0; i--) {
-    let root = document.createElement('div')
+    let Component = value
+    for (let i = elements.length - 1; i >= 0; i--) {
+        let root = document.createElement('div')
 
-    root.dataset.react = ''
-    root.dataset.component = key
-    elements[i].parentNode.insertBefore(root, elements[i])
-    root.appendChild(elements[i])
+        root.dataset.react = ''
+        root.dataset.component = key
+        elements[i].parentNode.insertBefore(root, elements[i])
+        root.appendChild(elements[i])
 
-    let html = elements[i].innerHTML || null
-    let props = getPropsFromAttributes(elements[i])
+        let html = elements[i].innerHTML || null
+        let props = getPropsFromAttributes(elements[i])
 
-    root = createRoot(root)
-    root.render(
-      <Suspense fallback={<div>Loading...</div>}>
-        <Component {...props}>
-          {html}
-        </Component>
-      </Suspense>
-    )
-  }
+        root = createRoot(root)
+        root.render(
+            <Suspense fallback={<div>Loading...</div>}>
+                <Component {...props}>
+                    {html}
+                </Component>
+            </Suspense>
+        )
+    }
 })
