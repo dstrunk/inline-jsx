c522e61654281bab0ad000883a5e4666f2715ea1
Daniel Strunk <daniel@strunk.me>
2022-10-14 14:03:40 -0400

    Wrap test render/updates in an act() call to simulate browser functionality

--- __tests__/MiniCart.test.js
+++ __tests__/MiniCart.test.js
@@ -1,4 +1,5 @@
 import { cleanup, render, screen } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 import userEvent from "@testing-library/user-event";
 import MiniCart from "../src/js/components/MiniCart";
 import eventBus from "../src/js/eventBus";
@@ -7,7 +8,9 @@ afterEach(cleanup)
 
 describe('MiniCart', () => {
   it('renders "Your shopping cart is empty." if no items are in the cart', async () => {
-    render(<MiniCart />)
+    act(() => {
+      render(<MiniCart />)
+    })
 
     await userEvent.click(screen.getByTestId('minicart-button'))
 
@@ -16,12 +19,16 @@ describe('MiniCart', () => {
   })
 
   it('renders items in the cart', async () => {
-    render(<MiniCart />)
+    act(() => {
+      render(<MiniCart />)
+    })
 
-    eventBus.publish('MINICART_ADD', {
-      product: { id: 1, title: 'Lorem Ipsum', price: 12.99 },
-      quantity: 1,
-    });
+    act(() => {
+      eventBus.publish('MINICART_ADD', {
+        product: { id: 1, title: 'Lorem Ipsum', price: 12.99 },
+        quantity: 1,
+      });
+    })
 
     await userEvent.click(screen.getByTestId('minicart-button'))
 
